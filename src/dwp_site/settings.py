545c8e8fe7b5f1f272445510adfdfc307403bab0
tang <sirisuke_t@silpakorn.edu>
2023-04-03 21:10:47 +0700

    add MEDIA

--- src/dwp_site/settings.py
+++ src/dwp_site/settings.py
@@ -148,6 +148,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
